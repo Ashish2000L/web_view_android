4fa6fc738b264183957356f6652898419c8fc201
Ashish2000L <ashishkumar28april@gmail.com>
2020-05-09 23:01:28 +0530

    updated splash activity
    added new splash animation and applied thread

--- app/src/main/java/com/example/web_view/splash.java
+++ app/src/main/java/com/example/web_view/splash.java
@@ -10,9 +10,11 @@ import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 import java.util.Objects;
 
@@ -21,8 +23,8 @@ import static java.lang.Thread.sleep;
 public class Splash extends AppCompatActivity {
 
     TextView text;
-    Animation anim_splash,anim,anim_appear;
-    ImageView splashimg;
+    ProgressBar progress;
+    Boolean status=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Window window=getWindow();
@@ -32,12 +34,14 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         text=findViewById(R.id.welcome);
+        progress=findViewById(R.id.progress);
+        /*
         anim_splash= AnimationUtils.loadAnimation(this,R.anim.animation);
-        anim=AnimationUtils.loadAnimation(this,R.anim.lift_from_bottom);
+       anim=AnimationUtils.loadAnimation(this,R.anim.lift_from_bottom);
         anim_appear= AnimationUtils.loadAnimation(this,R.anim.appear);
         splashimg=findViewById(R.id.splash);
         splashimg.startAnimation(anim_splash);
-        splashimg.animate().translationY(-2000).setDuration(2000).setStartDelay(4000);
+       splashimg.animate().translationY(-2000).setDuration(2000).setStartDelay(4000);
         text.setVisibility(View.VISIBLE);
         text.startAnimation(anim_appear);
         text.animate().scaleXBy(2).setStartDelay(7000).setDuration(2000);
@@ -61,6 +65,24 @@ public class Splash extends AppCompatActivity {
 
             }
         });
-        thread.start();
+        thread.start();*/
+        Thread to_web_view=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    startActivity(new Intent(Splash.this,web_view.class));
+                    //overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                    Animatoo.animateFade(Splash.this);
+                    finish();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        to_web_view.start();
+
     }
 }
