2145763d59f17fdb9d1c0a86abdf7dbe8e3e933d
Ashish2000L <ashishkumar28april@gmail.com>
2020-05-31 22:53:08 +0530

    added appupdater to splash.java file

--- app/src/main/java/com/example/web_view/splash.java
+++ app/src/main/java/com/example/web_view/splash.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -24,6 +25,12 @@ import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.AppUpdaterUtils;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
+import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+import com.github.javiersantos.appupdater.objects.Update;
 import com.google.android.play.core.appupdate.AppUpdateInfo;
 import com.google.android.play.core.appupdate.AppUpdateManager;
 import com.google.android.play.core.install.model.AppUpdateType;
@@ -68,30 +75,59 @@ public class Splash extends AppCompatActivity {
         });
         time.start();
         loading.setVisibility(View.VISIBLE);
-        Thread to_web_view=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    sleep(8000);
-                    Intent intent=new Intent(Splash.this,web_view.class);
-                    Animatoo.animateFade(Splash.this);
-                    //to resolve problem starting app each time from starting
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    //overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
 
-                    startActivity(intent);
-                    finish();
+        final Intent intent=new Intent(Splash.this,web_view.class);
+        Animatoo.animateFade(Splash.this);
+        //to resolve problem starting app each time from starting
 
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
 
-            }
-        });
-        to_web_view.start();
+        final AppUpdaterUtils appUpdaterUtils = new AppUpdaterUtils(this);
+        final AppUpdater appUpdater = new AppUpdater(this)
+                .setDisplay(Display.NOTIFICATION)
+                .setUpdateFrom(UpdateFrom.JSON)
+                .setUpdateJSON("http://free4all.ezyro.com/json_files/web_view.json")
+                .showAppUpdated(false)
+                .setTitleOnUpdateAvailable("Update available")
+                .setContentOnUpdateAvailable("Check out the latest version available of my app!")
+                .setTitleOnUpdateNotAvailable("Update not available")
+                .setContentOnUpdateNotAvailable("no update available!!")
+                .setButtonUpdate("Update now?")
+                .setButtonUpdateClickListener(new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                        appUpdaterUtils.setUpdateFrom(UpdateFrom.JSON);
+                        appUpdaterUtils.setUpdateJSON("http://free4all.ezyro.com/json_files/web_view.json");
+                        appUpdaterUtils.withListener(new AppUpdaterUtils.UpdateListener() {
+                                    @Override
+                                    public void onSuccess(Update update, Boolean isUpdateAvailable) {
+                                        Log.d("Latest Version", update.getLatestVersion());
+                                        Log.d("Latest Version Code", update.getLatestVersionCode().toString());
+                                        Log.d("URL", update.getUrlToDownload().toString());
+                                        Log.d("Is update available?", Boolean.toString(isUpdateAvailable));
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+
+                                    @Override
+                                    public void onFailed(AppUpdaterError error) {
+                                        Log.d("AppUpdater Error", "Something went wrong");
+                                    }
+                                });
+                        appUpdaterUtils.start();
+                    }
+                })
+                .setButtonDismiss("Maybe later")
+                .setButtonDismissClickListener(null)
+                .setButtonDoNotShowAgain(null)
+                .setCancelable(false);
+        appUpdater.start();
+
+
     }
-
 
 }
