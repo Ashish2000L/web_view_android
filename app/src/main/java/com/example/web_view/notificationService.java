7d6fc35a3ebae71275b72876d87b9dc1528645a2
Ashish2000L <ashishkumar28april@gmail.com>
2020-06-20 17:11:07 +0530

    pushing notification added through firebase to the users device

---
+++ app/src/main/java/com/example/web_view/notificationService.java
@@ -0,0 +1,56 @@
+package com.example.web_view;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class notificationService extends FirebaseMessagingService {
+
+    public static  int NOTIFICATION_ID=1;
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        generateNotification(remoteMessage.getNotification().getBody(),remoteMessage.getNotification().getTitle());
+
+    }
+
+    private void generateNotification(String body, String title) {
+
+        Intent intent = new Intent(this,Splash.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivities(this,0, new Intent[]{intent},PendingIntent.FLAG_ONE_SHOT);
+
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Notification notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_google)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(uri)
+                .setContentIntent(pendingIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if(NOTIFICATION_ID>1073741872)
+        {
+            NOTIFICATION_ID=0;
+        }
+
+        assert notificationManager != null;
+        notificationManager.notify(NOTIFICATION_ID++,notificationBuilder);
+    }
+}
